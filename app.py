2eec36d73c8e10900aa67f4719773efa94638112
Yongjiang Shi <yongjiang.shi@student.unimelb.edu.au>
2020-09-15 19:22:50 +1000

    update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 # web app that anglyse image content and translate into selected language
+from flask import Flask
+
+app = Flask(__name__)
+
+# Define a route for the app's home page
+@app.route("/")
+def index():
+    return "<h1>This the home page</h1>"
+
+# Define a route for the app's About page
+@app.route("/about")
+def about():
+    return "<h1>This the About page</h1>"
+
+# Define a route for the app's Contact Us page
+@app.route("/contact")
+def contact():
+    return "<h1>This the Contact Us page</h1>"
